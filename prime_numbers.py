9e8b167d079a0ed195cd7c80be39a99b6607eadd
daguy5134 <quentin16666@g.lfis.edu.hk>
2024-10-21 20:42:31 +0800

    started adding loops and other necessary features

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,7 +1,14 @@
 print("""The program will start at 0.
 At what number should it end ?""")
-range = input(">")
-while not range.isdigit() or range != 0 or range != 1:
-  print("You have to enter a positive number with no decimal greater than one.")
-  range = input(">")
-prime_numbers = [True]
+repeat_range = input(">")
+while not repeat_range.isdigit() or range != 0 or range != 1:
+    print("You have to enter a positive number with no decimal greater than one.")
+    repeat_range = input(">")
+prime_numbers = [True] * (int(repeat_range) + 1)
+for base_number in range(0, int(repeat_range)):
+    if prime_numbers[base_number]:
+        for multiples in range(base_number ** 2, int(repeat_range) // base_number + 1):
+        
+
+
+exit()
